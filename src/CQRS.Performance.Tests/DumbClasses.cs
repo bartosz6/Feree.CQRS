3c47dbf1946157a8815dfdede7f4ceee038e3920
Bartosz Kowalczyk <kontakt@feree.pl>
2017-12-02 11:39:54 +0100

    performance tests - gc

---
+++ src/CQRS.Performance.Tests/DumbClasses.cs
@@ -0,0 +1,52 @@
+﻿using CQRS.Core;
+
+namespace CQRS.Performance.Tests
+{
+    internal abstract class QueryHandler<T> : IQueryHandler<T, byte> where T : IQuery
+    {
+        public byte Handle(T query) => 0;
+    }
+    
+    internal class DumbClasses
+    {
+        internal class Query1 : IQuery {}
+
+        internal class Query2 : IQuery {}
+
+        internal class Query3 : IQuery {}
+
+        internal class Query4 : IQuery {}
+
+        internal class Query5 : IQuery {}
+
+        internal class Query6 : IQuery {}
+
+        internal class Query7 : IQuery {}
+
+        internal class Query8 : IQuery {}
+
+        internal class Query9 : IQuery {}
+
+        internal class Query10 : IQuery {}
+
+        internal class QueryHandler1 : QueryHandler<Query1> {}
+
+        internal class QueryHandler2 : QueryHandler<Query2> {}
+
+        internal class QueryHandler3 : QueryHandler<Query3> {}
+
+        internal class QueryHandler4 : QueryHandler<Query4> {}
+
+        internal class QueryHandler5 : QueryHandler<Query5> {}
+
+        internal class QueryHandler6 : QueryHandler<Query6> {}
+
+        internal class QueryHandler7 : QueryHandler<Query7> {}
+
+        internal class QueryHandler8 : QueryHandler<Query8> {}
+
+        internal class QueryHandler9 : QueryHandler<Query9> {}
+
+        internal class QueryHandler10 : QueryHandler<Query10> {}
+    }
+}
--- src/CQRS.Performance.Tests/Program.cs
+++ src/CQRS.Performance.Tests/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
 using CQRS.Core;
 
@@ -8,81 +11,37 @@ using CQRS.Core;
 namespace CQRS.Performance.Tests
 {
     [CoreJob]
+    [Config("columns=AllStatistics")]
+    [MemoryDiagnoser]
     public class Program
     {
-        private QueryDispatcher _queryDispatcher;
+        private readonly QueryDispatcher _queryDispatcher;
 
         public static void Main()
         {
             BenchmarkRunner.Run<Program>();
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Dispatch query, 1 out of 10")]
         public void DispatchTest()
         {
-            _queryDispatcher.Dispatch<Query6, string>(new Query6());
+            _queryDispatcher.Dispatch<DumbClasses.Query6, byte>(new DumbClasses.Query6());
         }
 
-        [IterationSetup]
-        public void Setup()
+        public Program()
         {
             _queryDispatcher = new QueryDispatcher(
-                new QueryHandler1(), 
-                new QueryHandler2(),
-                new QueryHandler3(),
-                new QueryHandler4(),
-                new QueryHandler5(),
-                new QueryHandler6(),
-                new QueryHandler7(),
-                new QueryHandler8(),
-                new QueryHandler9(),
-                new QueryHandler10(),
-                new QueryHandler11(),
-                new QueryHandler12(),
-                new QueryHandler13(),
-                new QueryHandler14(),
-                new QueryHandler15()
+                new DumbClasses.QueryHandler1(), 
+                new DumbClasses.QueryHandler2(),
+                new DumbClasses.QueryHandler3(),
+                new DumbClasses.QueryHandler4(),
+                new DumbClasses.QueryHandler5(),
+                new DumbClasses.QueryHandler6(),
+                new DumbClasses.QueryHandler7(),
+                new DumbClasses.QueryHandler8(),
+                new DumbClasses.QueryHandler9(),
+                new DumbClasses.QueryHandler10()
                 );
         }
-
-        private const string X = "x";
-        
-        public abstract class QueryHandler<T> : IQueryHandler<T, string> where T : IQuery
-        {
-            public string Handle(T query) => X; //$"{typeof(T).Name} {DateTime.UtcNow.Ticks}";
-        }
-
-        public class Query1 : IQuery {}
-        public class Query2 : IQuery {}
-        public class Query3 : IQuery {}
-        public class Query4 : IQuery {}
-        public class Query5 : IQuery {}
-        public class Query6 : IQuery {}
-        public class Query7 : IQuery {}
-        public class Query8 : IQuery {}
-        public class Query9 : IQuery {}
-        public class Query10 : IQuery {}
-        public class Query11 : IQuery {}
-        public class Query12 : IQuery {}
-        public class Query13 : IQuery {}
-        public class Query14 : IQuery {}
-        public class Query15 : IQuery {}
-
-        public class QueryHandler1 : QueryHandler<Query1> {}
-        public class QueryHandler2 : QueryHandler<Query2> {}
-        public class QueryHandler3 : QueryHandler<Query3> {}
-        public class QueryHandler4 : QueryHandler<Query4> {}
-        public class QueryHandler5 : QueryHandler<Query5> {}
-        public class QueryHandler6 : QueryHandler<Query6> {}
-        public class QueryHandler7 : QueryHandler<Query7> {}
-        public class QueryHandler8 : QueryHandler<Query8> {}
-        public class QueryHandler9 : QueryHandler<Query9> {}
-        public class QueryHandler10 : QueryHandler<Query10> {}
-        public class QueryHandler11 : QueryHandler<Query11> {}
-        public class QueryHandler12 : QueryHandler<Query12> {}
-        public class QueryHandler13 : QueryHandler<Query13> {}
-        public class QueryHandler14 : QueryHandler<Query14> {}
-        public class QueryHandler15 : QueryHandler<Query15> {}
-
     }
 }
